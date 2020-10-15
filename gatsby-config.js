d9837f6400ea3d902780a832e206aa5161f1583a
shota <xyz.shotao@gmail.com>
2020-10-15 23:06:46 +0900

    feat: SNS共有ボタンを設置する
    close #13

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,6 +79,7 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-twitter`,
+    `gatsby-plugin-styled-components`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
