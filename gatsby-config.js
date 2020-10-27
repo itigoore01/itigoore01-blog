40d38037f9e08e66dcee658b241ec63693fde6b6
itigoore01 <xyz.shotao@gmail.com>
2020-10-27 09:04:36 +0900

    refactor: gatsby-plugin-feed をリファクタ (#41)

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,8 +89,31 @@ module.exports = {
     {
       resolve: `gatsby-plugin-feed`,
       options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
         feeds: [
           {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
             query: `
               {
                 allMarkdownRemark(
@@ -111,8 +134,8 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
-            title: "shotaのブログ",
+            output: `/rss.xml`,
+            title: `shotaのブログ`,
           },
         ],
       },
